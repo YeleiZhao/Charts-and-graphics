9441b098a9fe97e5fdc2524c4eb5c214e7fef3c5
YeleiZhao <yeleizhao@gmail.com>
2018-02-01 17:44:52 +0800

    add a switch program
    dont know where to write the definition of the program

--- example1_1/example1_1/Form1.cs
+++ example1_1/example1_1/Form1.cs
@@ -22,11 +22,31 @@ namespace example1_1
         {
             Graphics g = e.Graphics;
             //Following codes shift the origin to the center of the client area,and then draw a line from (0,0) to (1,1)
-            g.PageUnit = GraphicsUnit.Inch;
+            g.PageUnit = GraphicsUnit.Inch;//指定英寸为度量单位（单位长度）
             g.TranslateTransform((ClientRectangle.Width / g.DpiX / 2), (ClientRectangle.Height / g.DpiY) / 2);
-            Pen greenPen = new Pen(Color.Green, 20 / g.DpiX);
-            g.DrawLine(greenPen, 0, 0, 2, 2);
+            //把坐标原点转换到窗口中心，把世界坐标系转换为页面坐标。每个传入的XY值均加上这个函数中的XY值。
+            Pen greenPen = new Pen(Color.Green, 1 / g.DpiX);//单个像素宽？
+            g.DrawLine(greenPen, 0, 0, 1, 1);
         }
+        /*       public Pen UnitScaling(Graphics g)
+               {
+                   switch (g.PageUnit)
+                   { 
+                       case GraphicsUnit.Pixel:
+                           return new Pen(Color.Black,, 1f);
+                       case GraphicsUnit.Inch:
+                           return new Pen(Color.Black, 1f/g.DpiX);
+                       case GraphicsUnit.Millimeter:
+                           return new Pen(Color.Black, 25.4f/ g.DpiX);
+                       case GraphicsUnit.Point:
+                           return new Pen(Color.Black, 72f / g.DpiX);
+                       case GraphicsUnit.Display:
+                           return new Pen(Color.Black, 75f / g.DpiX);
+                       case GraphicsUnit.Document:
+                           return new Pen(Color.Black, 300f / g.DpiX);
+                   }
+
+               }*/
     }
    
 }
